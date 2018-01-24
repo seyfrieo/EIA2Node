0de6b83f44284c35dad80fd1cf60892eff726071
seyfrieo <32566091+seyfrieo@users.noreply.github.com>
2018-01-24 18:31:38 +0100

    Update ServerTest.ts

--- ServerTest.ts
+++ ServerTest.ts
@@ -12,8 +12,8 @@ namespace ServerTest {
     // Port vom Process-Objekt erfragen 
     let port: number = process.env.PORT;
     // Port nicht definiert -> lokale Maschine, Port selbst definieren
-    if (port == undefined)
-        port = 8100;
+    //if (port == undefined)
+    //    port = 8100;
     
     // Server-Objekt kreieren
     let server: Http.Server = Http.createServer();
@@ -37,17 +37,17 @@ namespace ServerTest {
         _response.setHeader("Access-Control-Allow-Origin", "*");
         
         // Response-Body
-        _response.write("Ich höre Stimmen!!<br>");
-        _response.write("Port: " + port + "<br>");
-        _response.write("Method: " + _request.method + "<br>");
-        _response.write("Url: " + _request.url + "<br>");
-        _response.write("Headers: " + _request.headers + "<br>");
+        //_response.write("Ich höre Stimmen!!<br>");
+        //_response.write("Port: " + port + "<br>");
+        //_response.write("Method: " + _request.method + "<br>");
+        //_response.write("Url: " + _request.url + "<br>");
+        //_response.write("Headers: " + _request.headers + "<br>");
 
         // ?
         let query: AssocStringString = Url.parse(_request.url, true).query;
         // ?
         for (let key in query)
-            _response.write(key + ": " + query[key]);
+            _response.write(key + ": " + query[key]+"<br>");
         
         // Antwort abschließen und abschicken
         _response.end();
